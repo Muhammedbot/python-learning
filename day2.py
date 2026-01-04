7782f06d6a2b204665ca9a5c72b91293f6bcf7ce
Muhammed Jamiu <157972393+Muhammedbot@users.noreply.github.com>
2026-01-04 09:18:12 +0100

    day2.py
    day 2 learning variables and calculations

---
+++ day2.py
@@ -0,0 +1,18 @@
+name = "muhammed"
+age = 25
+city = "lagos"
+favorite_food = "rice"
+hobby = "reading"
+
+print("my name is " + name)
+print("i am " + str(age) + " years old")
+print("i live in " + city)
+print("my favorite food is " + favorite_food)
+print("my hobby is " + hobby)
+
+#calculate age in days
+age_in_days = age * 365
+print("i am " + str(age_in_days) + " days old!")
+
+
+day 2 variablles and calculation

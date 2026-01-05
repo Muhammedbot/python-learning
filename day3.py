050d60230d66438bf9b15323abf2c104437d6a88
Muhammed Jamiu <157972393+Muhammedbot@users.noreply.github.com>
2026-01-05 07:19:11 +0100

    day3 calculator program

---
+++ day3.py
@@ -0,0 +1,20 @@
+# Day 3 - simple calculator
+
+print("===== simple calculator =====")
+print()
+
+#get two number from user
+num1 = 10 # we'll hardcore for now 
+num2 = 5
+
+# Perform calculations
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 - num2
+division = num1 / num2
+
+# display results
+print(f"{num1} + {num2} = {addition} ")
+print(f"{num1} - {num2} = {substraction} ")
+print(f"{num1} * {num2} = {multiplication} ")
+print(f"{num1} / {num2} = {division} ")
